d55a7ed45308627c8ff9c584ab2218993f85a16e
viny <viny0698@gmail.com>
2023-11-03 14:43:55 +0530

    create views and url mappings

--- librarymanagement/urls.py
+++ librarymanagement/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
-from . import views
+from .views import books
+from .views import members
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("members", members.index, name="index"),
+    path("members/<int:member_id>", members.crud_members),
+    path(
+        "members/<int:member_id>/outstanding_transactions",
+        members.get_outstanding_transactions,
+    ),
+    path("members/<int:member_id>/issue_book", members.issue_book),
+    path("members/<int:member_id>/return_book", members.return_book),
+    path("members/<int:member_id>/charge_fee", members.charge_fee),
+    path("books", books.index),
+    path("books/<int:book_id>", books.crud_book),
 ]
---
+++ librarymanagement/views/index.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Welcome to My Library!")
+
+
+# Create your views here.
